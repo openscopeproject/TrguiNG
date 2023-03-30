fd2025a3aecefdfeeeb66c950bee9906b8976d05
qu1ck <anlutsenko@gmail.com>
2023-03-29 20:47:05 -0700

    Tweak mantine theme fonts

--- src/components/mantinetheme.tsx
+++ src/components/mantinetheme.tsx
@@ -1,7 +1,36 @@
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { ColorScheme, ColorSchemeProvider, MantineProvider, MantineThemeOverride } from "@mantine/core";
 import { useColorScheme } from "@mantine/hooks";
 import React from "react";
 import { useState } from "react";
+
+const Theme: (colorScheme: ColorScheme) => MantineThemeOverride = (colorScheme) => ({
+    colorScheme,
+    fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    headings: {
+        fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    },
+    components: {
+        Table: {
+            styles: {
+                root: {
+                    "& tbody tr td": {
+                        padding: "0 0.5rem"
+                    }
+                }
+            }
+        },
+    },
+    colors: {
+        secondaryColorName: ['#dcfdff', '#b2f4fd', '#85ebf9', '#58e3f6', '#36d9f3', '#25c0d9', '#1696aa', '#066b7a', '#00404a', '#00171b']
+    },
+    spacing: {
+        xs: '0.3rem',
+        sm: '0.4rem',
+        md: '0.5rem',
+        lg: '0.7rem',
+        xl: '1rem',
+    },
+});
 
 export function CustomMantineProvider({ children }: { children: React.ReactNode }) {
     const preferredColorScheme = useColorScheme();
@@ -11,32 +40,7 @@ export function CustomMantineProvider({ children }: { children: React.ReactNode 
 
     return (
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider withGlobalStyles withNormalizeCSS
-                theme={{
-                    colorScheme,
-                    components: {
-                        Table: {
-                            styles: {
-                                root: {
-                                    "& tbody tr td": {
-                                        padding: "0 0.5rem"
-                                    }
-                                }
-                            }
-                        },
-                    },
-                    colors: {
-                        secondaryColorName: ['#dcfdff', '#b2f4fd', '#85ebf9', '#58e3f6', '#36d9f3', '#25c0d9', '#1696aa', '#066b7a', '#00404a', '#00171b']
-                    },
-                    spacing: {
-                        xs: '0.3rem',
-                        sm: '0.4rem',
-                        md: '0.5rem',
-                        lg: '0.7rem',
-                        xl: '1rem',
-                    },
-                }}
-            >
+            <MantineProvider withGlobalStyles withNormalizeCSS theme={Theme(colorScheme)}>
                 {children}
             </MantineProvider>
         </ColorSchemeProvider>
