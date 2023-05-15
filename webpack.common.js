34bd4b014f2150f793860c028d1096399bfa7772
qu1ck <anlutsenko@gmail.com>
2023-05-15 16:19:14 -0700

    Split webpack config by env

--- webpack.common.js
+++ webpack.common.js
@@ -16,97 +16,80 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const path = require('path');
-const { compilerOptions } = require('./tsconfig.json');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { compilerOptions } = require("./tsconfig.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/index.tsx',
-    mode: 'development',
-    //mode: 'production',
+    entry: "./src/index.tsx",
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Transmission Remote GUI',
-            template: 'src/index.html',
+            title: "Transmission Remote GUI",
+            template: "src/index.html",
         }),
         new MiniCssExtractPlugin({
             filename: "[name].bundle.css"
         }),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            openAnalyzer: false,
-            reportFilename: path.resolve(__dirname, 'webpack-report.html')
-        }),
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: [".tsx", ".ts", ".js"],
         modules: [
-            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, "node_modules"),
             path.resolve(__dirname, compilerOptions.baseUrl),
         ],
     },
-    cache: false,
+    // cache: false,
     stats: {
-        preset: 'normal',
+        preset: "normal",
         modulesSpace: 35,
-        modulesSort: '!size',
+        modulesSort: "!size",
         groupModulesByPath: false,
         groupModulesByExtension: false,
-        //moduleAssets: false,
-        //orphanModules: true,
+        // moduleAssets: false,
+        // orphanModules: true,
     },
     optimization: {
-        // minimize: false,
         splitChunks: {
             // include all types of chunks
-            chunks: 'all',
+            chunks: "all",
             cacheGroups: {
                 react: {
                     test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-                    name: 'react',
+                    name: "react",
                 },
                 mantine: {
                     test: /[\\/]node_modules[\\/]@mantine[\\/]/,
-                    name: 'mantine',
+                    name: "mantine",
                 },
                 icons: {
                     test: /[\\/]node_modules[\\/]react-bootstrap-icons[\\/]/,
-                    name: 'bootstrap-icons',
+                    name: "bootstrap-icons",
                 },
                 other: {
                     test: /[\\/]node_modules[\\/]/,
                     priority: -1,
-                    name: 'other',
+                    name: "other",
                 },
             },
-        },
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: './dist',
-        client: {
-            overlay: true,
-            progress: true,
         },
     },
 };
---
+++ webpack.dev.js
@@ -0,0 +1,41 @@
+/**
+ * transgui-ng - next gen remote GUI for transmission torrent daemon
+ * Copyright (C) 2022  qu1ck (mail at qu1ck.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = merge(common, {
+    mode: "development",
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false,
+            reportFilename: path.resolve(__dirname, "webpack-report.html")
+        }),
+    ],
+    devtool: "source-map",
+    devServer: {
+        static: "./dist",
+        client: {
+            overlay: true,
+            progress: true,
+        },
+    },
+});
