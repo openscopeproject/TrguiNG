7faf82b4412affc34f2cee42a02c2b1cd73a9253
qu1ck <anlutsenko@gmail.com>
2023-12-29 08:02:58 -0800

    Add npm to arch script makedepends

--- ci/generate_pkgbuild.py
+++ ci/generate_pkgbuild.py
@@ -30,7 +30,7 @@ url="https://github.com/openscopeproject/TrguiNG"
 arch=('x86_64')
 license=('AGPL-3.0')
 depends=('alsa-lib' 'cairo' 'desktop-file-utils' 'fontconfig' 'gdk-pixbuf2' 'glib2' 'gtk3' 'hicolor-icon-theme' 'libayatana-appindicator' 'libsoup' 'openssl' 'webkit2gtk')
-makedepends=('rust>=1.69.0' 'nodejs>=16.0.0')
+makedepends=('rust>=1.69.0' 'nodejs>=16.0.0' 'npm')
 source=("git+https://github.com/openscopeproject/TrguiNG#tag=v$pkgver"
         "https://github.com/openscopeproject/TrguiNG/releases/download/v$pkgver/dbip.mmdb"
         "TrguiNG.desktop"::"https://raw.githubusercontent.com/flathub/org.openscopeproject.TrguiNG/master/org.openscopeproject.TrguiNG.desktop")
