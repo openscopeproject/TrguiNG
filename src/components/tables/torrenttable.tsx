aa68c23987dbde58eca96e98c4ad6c022b8f25d5
Oleksandr Burdun <a.burdun@gmail.com>
2024-01-05 19:22:32 +0200

    Revert infinity sign

--- src/components/tables/torrenttable.tsx
+++ src/components/tables/torrenttable.tsx
@@ -219,9 +219,11 @@ function PositiveNumberField(props: TableFieldProps) {
 function UploadRatioField(props: TableFieldProps) {
     return (
         <div style={{ width: "100%", textAlign: "right" }}>
-            {props.torrent.uploadedEver === 0
-                ? "-"
-                : (props.torrent.uploadedEver / props.torrent.downloadedEver).toFixed(2)}
+            {props.torrent.downloadedEver === 0
+                ? "∞"
+                : props.torrent.uploadedEver === 0
+                    ? "-"
+                    : (props.torrent.uploadedEver / props.torrent.downloadedEver).toFixed(2)}
         </div>
     );
 }
